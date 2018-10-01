22353d02d3e0a32131bb7585d973900976f1a9fc
lsj <jaef77@naver.com>
2018-10-02 04:02:47 +0900

    Edit main.cpp - 01

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+#include <net/if.h>
+#include <pcap.h>
 
 typedef struct arphdr{
     uint16_t hw_type;
@@ -14,33 +17,44 @@ typedef struct arphdr{
 }arphdr;
 
 
-void check_my_add(uint8_t *my_mac, uint32_t *my_ip)
+int check_my_add(uint8_t *my_mac, struct in_addr *my_ip, const char *interface)
 {
+    struct ifreq buf;
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     if(sock < 0)
+    {
         perror("ERROR : socket!");
-    struct ifreq buf;
-    memset(&buf, 0, sizeof(buf));
-    strcpy(buf.ifr_name, "eth0");
+        return -1;
+    }
+
+    strncpy(buf.ifr_name, interface, IFNAMSIZ-1);
+
     // MAC Address
     if(ioctl(sock, SIOCGIFHWADDR, &buf) < 0)
+    {
         perror("ERROR : ioctl - MAC!");
-    strncpy(my_mac, buf.ifr_hwaddr.sa_data, sizeof(buf.ifr_hwaddr.sa_data));
+        return -1;
+    }
+    for(int i=0;i<6;i++)
+        my_mac[i] = buf.ifr_hwaddr.sa_data[i];
 
     //IP Address
     if(ioctl(sock, SIOCGIFADDR, &buf) < 0)
+    {
         perror("ERROR : ioctl - IP");
+        return -1;
+    }
     *my_ip = ((struct sockaddr_in *)&buf.ifr_addr)->sin_addr;
 
-    return;
+    return 0;
 }
 
-void victim_mac_req(uint8_t *v_mac, uint32_t v_ip)
+void victim_mac_req(uint8_t *v_mac, struct in_addr v_ip)
 {
 
 }
 
-void send_arp_request(uint8_t *my_mac, uint8_t *v_mac, uint32_t my_ip, uint32_t v_ip, uint32_t t_ip)
+void send_arp_req(uint8_t *my_mac, uint8_t *v_mac, struct in_addr my_ip, struct in_addr v_ip, struct in_addr t_ip)
 {
     const uint8_t *packet;
 }
@@ -58,10 +72,10 @@ int main(int argc, char *argv[])
 
     // USE CUSTOM ARP_HEADER
     arphdr *ARP;
-    uint32_t victim_ip, target_ip, my_ip;
+    struct in_addr victim_ip, target_ip, my_ip;
     uint8_t my_mac[6], victim_mac[6];
-    uint8_t errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* handle;
+    // uint8_t errbuf[PCAP_ERRBUF_SIZE];
+    // pcap_t* handle;
 
     // Set victim_ip, target_ip
     if(inet_pton(AF_INET, argv[2], &victim_ip) == 0)
@@ -70,12 +84,14 @@ int main(int argc, char *argv[])
         perror("ERROR : wrong INPUT IP Address! - target");
 
     // Get my local MAC / IP address
-    check_my_add(my_mac, &my_ip);
+    check_my_add(my_mac, &my_ip, argv[1]);
 
     // for debug
     printf("%s\n", inet_ntoa(my_ip));
     for(int i=0;i<6;i++)
         printf("%02X:", my_mac[i]);
+    printf("\n");
+
 
 
 
@@ -84,7 +100,7 @@ int main(int argc, char *argv[])
     // victim_mac_req(victim_mac, victim_ip);
 
     // Send ARP Request(False)
-    // send_arp_request(my_mac, victim_mac, my_ip, victim_ip, target_ip);
+    // send_arp_req(my_mac, victim_mac, my_ip, victim_ip, target_ip);
 
     return 0;
 }
